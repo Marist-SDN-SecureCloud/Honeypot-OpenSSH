ab6062ede3515237451f5b5252fdd7e2c9ff8113
dgisolfi <daniel.gisolfi1@marist.edu>
2019-04-07 19:13:05 -0400

    Added Honeynet Logging Schema to TCP Server

--- TcpServer/server.py
+++ TcpServer/server.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-
+import os
 import sys
+import uuid
 import socket
 import datetime
+from SendToQueue import *
 
 class Server:
     def __init__(self, port):
@@ -10,6 +12,16 @@ class Server:
         self.host = None
         self.port = port
         self.ver = 1.0
+        # Honeypot details
+        self.honeypot = 'SSH-Honeypot'
+        self.honeypot_version = os.getenv('VERSION', '02')
+        self.id = self.honeypot[:3] + self.honeypot_version
+        self.host_ip = os.getenv('VERSION', '10.11.17.23')
+        self.host_name = socket.gethostname()
+        self.pid = os.getpid()
+        self.hpid = uuid.uuid4()
+        self.method = 'SSH'
+        
         self.setupSocket()
 
     def setupSocket(self):
@@ -22,7 +34,6 @@ class Server:
             self.sock.bind(('', self.port))
         except OSError:
             print('Error Port already in use')
-            sys.exit(1)
        
 
     # Logging to ensure we arent losing any data
@@ -34,6 +45,14 @@ class Server:
         except:
             print('Server: Error logging to File!')
 
+    def buildLog(self, msg):
+        data = msg.split(',')
+        log = f'{self.id},{datetime.datetime.now()},{self.honeypot},{self.host_ip},'
+        log += f'{self.host_name},{self.pid},{self.hpid},{self.method},Null,'
+        log += f'{data[0]},{data[1]},NULL,username={data[2]}&password={data[3]}'
+
+        return log
+    
     # Handle all incoming client connections
     def handleConnections(self):
         print(f'SocketStream Server v{self.ver}')
@@ -52,7 +71,10 @@ class Server:
                 print(attack_log)
                 self.logger(attack_log)
 
-                conn.send(f'Message Received'.encode())
+                log = self.buildLog(msg)
+                print(log)
+                status = sendMsg(log)
+                conn.send(f'Message Received Status: {status}'.encode())
                 conn.close()
         except KeyboardInterrupt:
             print('Server: Shutting Down')
